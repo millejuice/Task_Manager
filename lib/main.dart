dc165bff23a1ef238096127a770f592798211889
Unduck <csc4592@gmail.com>
2023-05-20 06:36:26 +0900

    change app and main

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pixel_n_semicolon/lock_screen.dart';
 import 'notifi_service.dart';
-
 import 'home_screen.dart';
+import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService().initNotification();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  final bool flag = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Notifications',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'DungGeunMo',
-      ),
-      home: flag
-          ? const LockPage()
-          : const MyHomePage(title: 'Flutter Local Notifications'),
-    );
-  }
+  runApp(const PixelNSemicolon());
 }
